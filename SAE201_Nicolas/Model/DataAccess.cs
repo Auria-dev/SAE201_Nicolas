a0c93f9fb428251eac3eac2008006204ce39257c
Malak ISMAIL <ismaimal@iut-acy.local>
2025-06-05 16:40:34 +0200

    data access started

---
+++ SAE201_Nicolas/Model/DataAccess.cs
@@ -0,0 +1,24 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAE201_Nicolas.Model
+{
+    public class DataAccess
+    {
+        private static readonly DataAccess instance = new DataAccess();
+        private readonly string connectionString = "Host=srv-peda-new;Port=5433;Username=login;Password=motdepasse;Database=nomdb;Options='-c search_path=schema'";
+        private NpgsqlConnection connection;
+
+        public static DataAccess Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+    }
+}
