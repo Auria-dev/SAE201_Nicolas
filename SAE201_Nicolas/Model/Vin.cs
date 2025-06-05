cde86e6fd820a123bbd4982a604acfcaacd170f8
Malak ISMAIL <ismaimal@iut-acy.local>
2025-06-05 15:51:12 +0200

    vin

---
+++ SAE201_Nicolas/Model/Vin.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAE201_Nicolas.Model
+{
+    public class Vin
+    {
+        private int numVin;
+        private string nomVin;
+        private double prixVin;
+        private string descriptif;
+        private int annee;
+
+        public int NumVin
+        {
+            get
+            {
+                return this.numVin;
+            }
+
+            set
+            {
+                this.numVin = value;
+            }
+        }
+
+        public string NomVin
+        {
+            get
+            {
+                return this.nomVin;
+            }
+
+            set
+            {
+                this.nomVin = value;
+            }
+        }
+
+        public double PrixVin
+        {
+            get
+            {
+                return this.prixVin;
+            }
+
+            set
+            {
+                this.prixVin = value;
+            }
+        }
+
+        public string Descriptif
+        {
+            get
+            {
+                return this.descriptif;
+            }
+
+            set
+            {
+                this.descriptif = value;
+            }
+        }
+
+        public int Annee
+        {
+            get
+            {
+                return this.annee;
+            }
+
+            set
+            {
+                this.annee = value;
+            }
+        }
+    }
+}
