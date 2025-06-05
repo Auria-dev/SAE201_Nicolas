f3d65dcbd064c98765d9a960cb6b06536e25a6db
Julien BERST <berstj@iut-acy.local>
2025-06-05 15:39:27 +0200

    uwu

---
+++ SAE201_Nicolas/Model/Commande.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAE201_Nicolas.Model
+{
+    public enum EtatCommande
+    {
+        Validée,
+        En_attante,
+        Supprimé
+    }
+
+    public class Commande
+    {
+        private int numCommande;
+        private int numEmploye;
+        private DateTime dateCommande;
+        private EtatCommande etatCommande;
+        private double prixTotal;
+
+    }
+}
