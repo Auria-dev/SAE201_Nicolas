17e5ff3d1629231680c11d4b99f9fc3983f64cbd
Julien BERST <berstj@iut-acy.local>
2025-06-12 16:25:21 +0200

    no move MVVM :3

---
+++ SAE201_Nicolas/Model/GestionVin.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAE201_Nicolas.Model
+{
+    public class GestionVin
+    {
+        private string nom;
+        private ObservableCollection<Vin> lesVins;
+        private ObservableCollection<Demande> lesDemandes;
+        private ObservableCollection<Commande> lesCommandes;
+        private ObservableCollection<Fournisseur> lesFournisseurs;
+        private ObservableCollection<DetailCommande> lesDetailsCommandes;
+        private ObservableCollection<Employe> lesEmployes;
+        private ObservableCollection<Client> lesClients;
+
+        public GestionVin(string nom)
+        {
+            this.Nom = nom;
+            this.lesVins = new ObservableCollection<Vin>(new Vin().FindAll());
+            this.lesDemandes = new ObservableCollection<Demande>(new Demande().FindAll());
+            this.lesCommandes = new ObservableCollection<Commande>(new Commande().FindAll());
+            this.lesFournisseurs = new ObservableCollection<Fournisseur>(new Fournisseur().FindAll());
+            this.lesDetailsCommandes = new ObservableCollection<DetailCommande>(new DetailCommande().FindAll(this));
+            this.lesEmployes = new ObservableCollection<Employe>(new Employe().FindAll());
+            this.lesClients = new ObservableCollection<Client>(new Client().FindAll());
+
+            Console.WriteLine(
+                "Found:\n" +
+                this.lesEmployes.Count + " employes\n" +
+                this.lesVins.Count + " vins\n" +
+                this.lesDemandes.Count + " demandes\n" +
+                this.lesCommandes.Count + " commandes\n" +
+                this.lesFournisseurs.Count + " fournisseurs\n" +
+                this.lesDetailsCommandes.Count + " detailscommandes\n"
+            );
+        }
+
+        public GestionVin() : this("") { }
+
+        public string Nom
+        {
+            get { return this.nom; }
+            set { this.nom = value; }
+        }
+
+        public ObservableCollection<Vin> LesVins
+        {
+            get { return this.lesVins; }
+            set { this.lesVins = value; }
+        }
+
+        public ObservableCollection<Demande> LesDemandes
+        {
+            get { return this.lesDemandes; }
+            set { this.lesDemandes = value; }
+        }
+
+        public ObservableCollection<Commande> LesCommandes
+        {
+            get { return this.lesCommandes; }
+            set { this.lesCommandes = value; }
+        }
+
+        public ObservableCollection<Fournisseur> LesFournisseurs
+        {
+            get { return this.lesFournisseurs; }
+            set { this.lesFournisseurs = value; }
+        }
+
+        public ObservableCollection<DetailCommande> LesDetailsCommandes
+        {
+            get { return this.lesDetailsCommandes; }
+            set { this.lesDetailsCommandes = value; }
+        }
+
+        public ObservableCollection<Employe> LesEmployes
+        {
+            get { return this.lesEmployes; }
+            set { this.lesEmployes = value; }
+        }
+
+        public ObservableCollection<Client> LesClients
+        {
+            get
+            {
+                return this.lesClients;
+            }
+
+            set
+            {
+                this.lesClients = value;
+            }
+        }
+    }
+}
---
+++ SAE201_Nicolas/View/UC/AjouterVinUC.xaml.cs
@@ -0,0 +1,66 @@
+﻿using SAE201_Nicolas.Core;
+using SAE201_Nicolas.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SAE201_Nicolas.View.UC
+{
+    /// <summary>
+    /// Logique d'interaction pour AjouterVinUC.xaml
+    /// </summary>
+    public partial class AjouterVinUC : UserControl
+    {
+        public AjouterVinUC()
+        {
+            InitializeComponent();
+        }
+
+        public AjouterVinUC(Vin unVin)
+        {
+            InitializeComponent();
+            this.DataContext = unVin;
+        }
+
+        private void ClickedReturn(object sender, MouseButtonEventArgs e)
+        {
+            ViewManager.Instance.GoBack();
+        }
+
+        private void BtnAjouterVinValider(object sender, RoutedEventArgs e)
+        {
+            Vin unVin = new Vin();
+            unVin.NumFournisseur = cbFournisseur.SelectedIndex;
+            unVin.NumTypeVin = cbTypeVin.SelectedIndex;
+            unVin.NumAppelation = ComboxBoxAppellation.SelectedIndex;
+            unVin.NomVin = TxtboxNomVin.Text;
+            unVin.PrixVin = double.Parse(TxtboxPrixVin.Text);
+            unVin.Descriptif = "";
+            unVin.Annee = int.Parse(TxtboxAnnee.Text);
+            
+            unVin.NumVin = unVin.AjouterVin();
+
+            //Console.WriteLine("ComboxBoxAppellation = " + ComboxBoxAppellation.SelectedIndex);
+            //Console.WriteLine("cbFournisseur = " + cbFournisseur.SelectedIndex);
+            //Console.WriteLine("cbTypeVin = " + cbTypeVin.SelectedIndex);
+
+            Console.WriteLine("Num avant: " + MainWindow.LaGestionDeVins.LesVins.Count);
+            MainWindow.LaGestionDeVins.LesVins.Add(unVin);
+            Console.WriteLine("Num apres: " + MainWindow.LaGestionDeVins.LesVins.Count);
+
+            MessageBox.Show("Vin enregistré.", $"Insertion du nouveau vin réussite.", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
